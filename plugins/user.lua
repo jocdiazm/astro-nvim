1272d7bb059133eb3e6ef6f1de39cd33758573fe
Jose Carlos <jocdiazmuic@gmail.com>
2024-01-19 20:49:33 -0500

    adding which key and signature help

--- plugins/user.lua
+++ plugins/user.lua
@@ -22,4 +22,24 @@ return {
       },
     },
   },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "VeryLazy",
+    opts = {
+      doc_lines = 0, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 3000
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
