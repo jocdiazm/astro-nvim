0d39069a1351f3c01fb2652a1fa446fa6bfbf8de
Carlos Diaz <jdiazmunoz@provectus.com>
2024-04-02 12:06:38 -0500

    adding simple packs to community

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -6,7 +6,13 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 ---@type LazySpec
 return {
+  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  -- example of importing a plugin
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.python" },
 }
+-- import/override with your plugins folder
