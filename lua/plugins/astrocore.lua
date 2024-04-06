7cc0302faf35bdf6344161213f10a4ee68f667d6
jose carlos <jocdiazmuic@gmail.com>
2024-04-06 18:42:23 -0500

    disabling cursorline

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -27,6 +27,7 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
+        cursorline = false, --sets vim.opt.cursorlineStro
         swapfile = false, -- sets vim.opt.swapfile
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
