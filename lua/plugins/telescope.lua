f1c6880477a1173995ca7fb92ff9f3d6965a5a6e
Jose Carlos <jocdiazmuic@gmail.com>
2024-08-12 19:48:56 -0500

    updating and adding telescope native extensions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,6 +26,14 @@ return {
           "^vendor/",
         },
       },
+      extensions = {
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        },
+      },
     },
     keys = {
       { "<leader>fh", "<cmd>Telescope find_files hidden=true<cr>", desc = "Telescope Files Hidden" },
